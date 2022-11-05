efcbb4900bdb5a7cc039e76aab25f5e26cb5cf63
martinlopez1234 <martin.lr@hotmail.com>
2022-11-05 19:48:01 +0000

    My first commit

--- src/models.py
+++ src/models.py
@@ -8,23 +8,53 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class Users(Base):
+    __tablename__ = 'users'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    username = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    password = Column(String(250), nullable=False)
+    bio = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
+class Posts(Base):
+    __tablename__ = 'posts'
+    # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    photo = Column(String(250), nullable=False)
+    description = Column(String(250), nullable=False)
+    created_at = Column(String(250), nullable=False)
+    updated_at= Column(String(250), nullable=False)
+
+class Followers(Base):
+    __tablename__ = 'followers'
+   
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    accepted = Column(String(250), nullable=False)
+ 
+class Likes(Base):
+    __tablename__ = 'likes'
+   
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('users.id')) 
+    post_id = Column(Integer, ForeignKey("posts.id"))
+
+class Comments(Base):
+    __tablename__ = 'comments'
+   
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('users.id')) 
+    post_id = Column(Integer, ForeignKey("posts.id"))
+    content = Column(String(250), nullable=False)
+    created_at = Column(String(250), nullable=False)
+    updated_at= Column(String(250), nullable=False)
+
+
 
     def to_dict(self):
         return {}
